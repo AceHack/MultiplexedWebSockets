e44e1a824866c5c4043b93042234f4ebe9631805
Aaron Stainback <astainba@itron.com>
2018-10-16 15:30:49 -0400

    adding http/2 benchmarks as well

--- src/Benchmarks/Program.cs
+++ src/Benchmarks/Program.cs
@@ -2,20 +2,33 @@
 using System.Buffers;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net;
 using System.Net.WebSockets;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 using MultiplexedWebSockets.Server;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Microsoft.AspNetCore.Http;
 
 namespace MultiplexedWebSockets.Benchmarks
 {
+    enum Benchmark
+    {
+        MultiplexedWebSocket,
+        HttpClient,
+    }
+
     static class Program
     {
-        const string _serverUrl = "http://*:5000";
-        const string _clientUrl = "ws://localhost:5000";
+        const string _clientWebSocketUrl = "wss://localhost:5001/ws";
+        const string _httpClientUrl = "https://localhost:5001/client";
         const int _times = 100;
         const int _batch = 10000;
 
@@ -23,10 +36,18 @@ namespace MultiplexedWebSockets.Benchmarks
         {
             var cts = new CancellationTokenSource();
             var webHost = WebHost.CreateDefaultBuilder(args)
-                .UseUrls(_serverUrl)
+                .ConfigureKestrel(kestrelServerOptions =>
+                {
+                    kestrelServerOptions.Listen(IPAddress.Any, 5001, listenOptions =>
+                    {
+                        listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                        listenOptions.UseHttps();
+                    });
+                })
                 .Configure(app =>
                 {
-                    app.UseMultiplexedServerWebSocket();
+                    app.Map("/ws", HandleMultiplexedWebSocket);
+                    app.Map("/client", HandleHttpClient);
                 })
                 .ConfigureLogging(builder =>
                 {
@@ -36,28 +57,65 @@ namespace MultiplexedWebSockets.Benchmarks
                 .Build();
 
             var runTask = webHost.RunAsync(cts.Token);
-            var client = new ClientWebSocket();
-            await client.ConnectAsync(new Uri(_clientUrl), cts.Token).ConfigureAwait(false);
-            var mx = new MultiplexedWebSocket(client);
-            var data = new ReadOnlySequence<byte>(new byte[] { 1, 2, 3, 4, 5 });
-            var watch = Stopwatch.StartNew();
-            for (int i = 0; i < _times; i++)
+            using (var clientWebSocket = new ClientWebSocket())
+            using (var httpClient = new HttpClient(new WinHttpHandler(), true))
             {
-                var tasks = new List<Task>(_batch);
-                for (int b = 0; b < _batch; b++)
+                httpClient.BaseAddress = new Uri(_httpClientUrl);
+                await clientWebSocket.ConnectAsync(new Uri(_clientWebSocketUrl), cts.Token).ConfigureAwait(false);
+                var mx = new MultiplexedWebSocket(clientWebSocket);
+                var data = new ReadOnlySequence<byte>(new byte[] { 1, 2, 3, 4, 5 });
+                foreach (Benchmark benchmark in Enum.GetValues(typeof(Benchmark)))
                 {
-                    tasks.Add(mx.RequestResponseAsync(data, cts.Token));
+                    if (benchmark == Benchmark.HttpClient)
+                    {
+                        var response = await httpClient.SendAsync(new HttpRequestMessage() { Method = HttpMethod.Get, Version = HttpVersion.Version20 });
+                        if (response.Version != HttpVersion.Version20)
+                        {
+                            throw new InvalidOperationException("Should be Http/2");
+                        }
+                    }
+
+                    var watch = Stopwatch.StartNew();
+                    for (int i = 0; i < _times; i++)
+                    {
+                        var tasks = new List<Task>(_batch);
+                        for (int b = 0; b < _batch; b++)
+                        {
+                            switch (benchmark)
+                            {
+                                case Benchmark.MultiplexedWebSocket:
+                                    tasks.Add(mx.RequestResponseAsync(data, cts.Token));
+                                    break;
+                                case Benchmark.HttpClient:
+                                    tasks.Add(httpClient.SendAsync(new HttpRequestMessage() { Method = HttpMethod.Get, Version = HttpVersion.Version20 }));
+                                    break;
+                            }
+                        }
+
+                        await Task.WhenAll(tasks).ConfigureAwait(false);
+                    }
+
+                    watch.Stop();
+                    Console.WriteLine($"{benchmark}: {(_times * _batch) / watch.Elapsed.TotalSeconds:N0} per second");
                 }
-
-                await Task.WhenAll(tasks).ConfigureAwait(false);
             }
-
-            watch.Stop();
-            Console.WriteLine((_times * _batch) / watch.Elapsed.TotalSeconds);
             cts.Cancel();
             await runTask.ConfigureAwait(false);
             await webHost.StopAsync().ConfigureAwait(false);
             Console.ReadKey(true);
+        }
+
+        private static void HandleMultiplexedWebSocket(IApplicationBuilder app)
+        {
+            app.UseMultiplexedServerWebSocket();
+        }
+
+        private static void HandleHttpClient(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello");
+            });
         }
     }
 }
